5d56603864db9ce370b5b21fb8aa42ecd05b59d0
User <wsh715@gmail.com>
2025-06-29 20:15:53 +0900

    가상달력찍기

--- src/hsh/calendar/ForMaxDays.java
+++ src/hsh/calendar/ForMaxDays.java
@@ -8,6 +8,16 @@ public class ForMaxDays {
 	public int maxDaysOfMonth(int m) {
 		return maxDays[m-1];
 		}
+	public void printCalendar() {
+		System.out.println("Hello, Calendar!");
+		System.out.println("일  월  화  수  목  금  토");
+		System.out.println("--------------------");	
+		System.out.println("1  2  3  4  5  6  7");
+		System.out.println("8  9  10 11 12 13 14");
+		System.out.println("15 16 17 18 19 20 21");
+		System.out.println("22 23 24 25 26 27 28");
+	}
+	
 	public static void main(String[] args) {
 		
 		String PROMPT = "cal> ";
@@ -25,7 +35,9 @@ public class ForMaxDays {
 			if(m > 12) {
 				continue;
 			}
-			System.out.printf("%d월은 %d일까지 있습니다.\n", m, fmd.maxDaysOfMonth(m));
+//			System.out.printf("%d월은 %d일까지 있습니다.\n", m, fmd.maxDaysOfMonth(m));
+			
+			fmd.printCalendar();
 			}
 		
 		
