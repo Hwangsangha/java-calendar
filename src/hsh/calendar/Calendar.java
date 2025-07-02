43c22a8b7736377061cf714e1b1dcf2c88c850ef
User <wsh715@gmail.com>
2025-07-02 16:16:44 +0900

    윤년 달력 찍기

--- src/hsh/calendar/Calendar.java
+++ src/hsh/calendar/Calendar.java
@@ -4,19 +4,32 @@ import java.util.Scanner;
 
 //들여쓰기 자동정렬 : ctrl, shift + f
 public class Calendar {
-	private final int[] maxDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private final int[] max_Days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	private final int[] LEAP_MAX_DAYS = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 	
-	public int maxDaysOfMonth(int m) {
-		return maxDays[m-1];
+	public boolean isLeapYear(int year) {
+		if(year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			return true;
+		else
+			return false;
 	}
-	public void printCalendar(int m) {
+	
+	public int maxDaysOfMonth(int year, int m) {
+		if(isLeapYear(year)) {
+			return LEAP_MAX_DAYS[m - 1];
+		} else {
+			return max_Days[m-1];
+		}
+		
+	}
+	public void printCalendar(int year, int m) {
 		System.out.printf("<<%3d>>\n", m);
 		System.out.println("일  월  화  수  목  금  토");
 		System.out.println("--------------------");	
 		
-		int maxDay = maxDaysOfMonth(m);
+		int maxDay = maxDaysOfMonth(year, m);
 		
-		for(int i = 1; i < maxDay; i++) {
+		for(int i = 1; i <= maxDay; i++) {
 			System.out.printf("%3d",i);
 			if(i %7 == 0)
 				System.out.println();
@@ -37,7 +50,6 @@ public class Calendar {
 		Scanner sc = new Scanner(System.in);
 		int m = sc.nextInt();
 		
-		System.out.printf("%d월은 %d일까지 있습니다.\n", m, cal.maxDaysOfMonth(m));
 		
 		sc.close();
 	}
--- src/hsh/calendar/Prompt.java
+++ src/hsh/calendar/Prompt.java
@@ -11,18 +11,22 @@ public class Prompt {
 		Calendar cal = new Calendar();
 		
 		int m = 1;
+		int year = 2025;
 		
 		while (true) {
+			System.out.println("년도를 입력하세요.");
+			System.out.print(PROMPT);
+			year = sc.nextInt();
 			System.out.println("달을 입력하세요.");
 			System.out.print(PROMPT);
 			m = sc.nextInt();
-			if(m == -1) {
+			if(year == -1 || m == -1) {
 				break;
 			}
 			if(m > 12) {
 				continue;
 			}
-			cal.printCalendar(m);
+			cal.printCalendar(year, m);
 		}
 		
 		System.out.println("프로그램 종료");
