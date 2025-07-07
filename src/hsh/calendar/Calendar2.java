12ade8f5b284f38e569a47e2f9f8b648c71a0eb8
HwangEunHye <wsh715@gmail.com>
2025-07-07 20:54:39 +0900

    25/07/07

--- src/hsh/calendar/Calendar2.java
+++ src/hsh/calendar/Calendar2.java
@@ -20,39 +20,39 @@ public class Calendar2 {
 		}
 		
 	}
-	public void printCalendar(int year, int m, int weekday) {
-		System.out.printf("<<%3d>>\n", m);
-		System.out.println("일  월  화  수  목  금  토");
-		System.out.println("--------------------");	
-		
-		//print blank space
-		for(int i = 0; i < weekday; i++) {
-			System.out.print("   ");
-		}
-		int maxDay = maxDaysOfMonth(year, m);
-		int count = 7 - weekday;
-		int delim = (count < 7) ? count : 0;
-//		if (count > 7) {
-//			delim = count;
-//		}else {
-//			delim = 0;
+//	public void printCalendar(int year, int m, int weekday) {
+//		System.out.printf("<<%3d>>\n", m);
+//		System.out.println("일  월  화  수  목  금  토");
+//		System.out.println("--------------------");	
+//		
+//		//print blank space
+//		for(int i = 0; i < weekday; i++) {
+//			System.out.print("   ");
 //		}
-		
-		//print first line
-		for(int i = 1; i <= count; i++) {
-			System.out.printf("%3d", i);
-		}
-		System.out.println();
-		
-		count++;
-		for(int i = count; i <= maxDay; i++) {
-			System.out.printf("%3d",i);
-			if(i %7 == delim)
-				System.out.println();
-		}
-		System.out.println();
-		System.out.println();
-	}
+//		int maxDay = maxDaysOfMonth(year, m);
+//		int count = 7 - weekday;
+//		int delim = (count < 7) ? count : 0;
+////		if (count > 7) {
+////			delim = count;
+////		}else {
+////			delim = 0;
+////		}
+//		
+//		//print first line
+//		for(int i = 1; i <= count; i++) {
+//			System.out.printf("%3d", i);
+//		}
+//		System.out.println();
+//		
+//		count++;
+//		for(int i = count; i <= maxDay; i++) {
+//			System.out.printf("%3d",i);
+//			if(i %7 == delim)
+//				System.out.println();
+//		}
+//		System.out.println();
+//		System.out.println();
+//	}
 	
 	public void printCalendar(int year, int m) {
 		System.out.printf("   <<%d년% 3d월>>\n", year, m);
@@ -93,24 +93,33 @@ public class Calendar2 {
 
 	private int getWeekDay(int year, int m, int day) {
 		int syear = 1970;
-		int Sm = 1;
+		int smonth = 1;
 		int sday = 1;
-		int standardsweekday = 3; //목요일
+		int standardWeekday = 3; //목요일
 		
 		int count = 0;
 		
 		for (int i = syear; i <= year; i++) {
-			int delta = isLeapYear(year) ? 366 : 365;
+			int delta = isLeapYear(i) ? 366 : 365;
 			count += delta;
 		}
-		System.out.println(count);
-		return 0;
+		//System.out.println(count);
+		for(int i = 1; i < m; i++) {
+			int delta = maxDaysOfMonth(year, i);
+			count += delta;
+			
+		}
+		count += day -1;
+		
+		return count;
 			
 	}
 	
 	//simple test code here
 	public static void main(String[] args) {
 		Calendar2 c = new Calendar2();
-		c.getWeekDay(1970, 5, 1);
+		System.out.println(c.getWeekDay(1970, 1, 1) == 0);
+		System.out.println(c.getWeekDay(1970, 2, 1) == 31);
+		System.out.println(c.getWeekDay(1970, 3, 1) == 59);
 	}
 }
